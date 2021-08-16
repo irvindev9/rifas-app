872a5ed008f1c98bc2c2746925e11a48bb2ddd5c
Perla Arroyos <perla.pmar04@gmail.com>
2021-08-16 10:30:31 -0600

    Add except methods in tickebuyed middleware

--- app/Http/Controllers/TicketBuyedController.php
+++ app/Http/Controllers/TicketBuyedController.php
@@ -11,7 +11,7 @@ class TicketBuyedController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('verificator','show');;
     }
 
     /**
