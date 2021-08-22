d2d6a24d8dd1400983bf2ab59e49651e9c45cff4
Irvin López <irvin_21_95@hotmail.com>
2021-08-21 18:09:06 -0600

    fix data to long

--- database/migrations/2021_07_18_184557_create_settings_table.php
+++ database/migrations/2021_07_18_184557_create_settings_table.php
@@ -16,7 +16,7 @@ class CreateSettingsTable extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
             $table->string("code");
-            $table->text("content")->nullable();
+            $table->longText("content")->nullable();
             $table->timestamps();
         });
     }
--- public/js/app.js
+++ public/js/app.js
@@ -20596,7 +20596,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.slide {\n    -webkit-animation-name: slideAnimation;\n            animation-name: slideAnimation;\n    -webkit-animation-duration: 2s;\n            animation-duration: 2s;\n}\n.image-auto {\n    height: 300px!important;\n    width: auto;\n}\n@-webkit-keyframes slideAnimation {\nfrom {\n    transform: translate(-50px,0);\n    opacity: 0.5;\n}\nto {\n    transform: translate(0,0);\n    opacity: 1;\n}\n}\n@keyframes slideAnimation {\nfrom {\n    transform: translate(-50px,0);\n    opacity: 0.5;\n}\nto {\n    transform: translate(0,0);\n    opacity: 1;\n}\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.slide {\r\n    -webkit-animation-name: slideAnimation;\r\n            animation-name: slideAnimation;\r\n    -webkit-animation-duration: 2s;\r\n            animation-duration: 2s;\n}\n.image-auto {\r\n    height: 300px!important;\r\n    width: auto;\n}\n@-webkit-keyframes slideAnimation {\nfrom {\r\n    transform: translate(-50px,0);\r\n    opacity: 0.5;\n}\nto {\r\n    transform: translate(0,0);\r\n    opacity: 1;\n}\n}\n@keyframes slideAnimation {\nfrom {\r\n    transform: translate(-50px,0);\r\n    opacity: 0.5;\n}\nto {\r\n    transform: translate(0,0);\r\n    opacity: 1;\n}\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
