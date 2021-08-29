244b1992d78b99e6e334006a1b11266ddac2389d
Perla Arroyos <perla.pmar04@gmail.com>
2021-08-29 08:49:40 -0600

    Fix features in admin panel

--- resources/views/panel-admin/tickets-buyed/index.blade.php
+++ resources/views/panel-admin/tickets-buyed/index.blade.php
@@ -14,50 +14,52 @@
                     <div class="container">
                         <div class="row justify-content-center">
                             <div class="col-md-12">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">No. Boleto</th>
-                                            <th scope="col">Boletos extra</th>
-                                            <th scope="col">Nombre</th>
-                                            <th scope="col">Apellido</th>
-                                            <th scope="col">No. Whats</th>
-                                            <th scope="col">Estado</th>
-                                            <th scope="col">Estatus</th>
-                                            <th scope="col"></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($ticketsBuyed as $ticketBuyed)
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
                                             <tr>
-                                                <th scope="row">{{ $ticketBuyed->ticket }}</th>
-                                                <th>
-                                                    @foreach ($ticketBuyed->otherTicketsBuyed as $item)
-                                                        {{ $item->ticket }}
-                                                    @endforeach                  
-                                                </th>
-                                                <td>{{ $ticketBuyed->name_client }}</td>      
-                                                <td>{{ $ticketBuyed->lastname_client }}</td>                                      
-                                                <td>{{ $ticketBuyed->whats_number }}</td>                                      
-                                                <td>{{ $ticketBuyed->state }}</td>                                      
-                                                <td>
-                                                    @if($ticketBuyed->paid == 1)
-                                                        <strong class="text-success">Pagado</strong> 
-                                                    @else
-                                                        <strong class="text-danger">No pagado</strong>     
-                                                    @endif
-                                                </td>                                      
-                                                <td>
-                                                    <a class="btn btn-outline-secondary btn-sm btn-block mb-1" href="{{ route('ticketsBuyed.edit', $ticketBuyed) }}" role="button">Editar</a>
-                                                    <form method="POST" action="{{ route('ticketsBuyed.delete', $ticketBuyed) }}" class="">
-                                                        @csrf
-                                                        <button class="btn btn-outline-danger btn-sm btn-block">Eliminar</button>
-                                                    </form>
-                                                </td>
+                                                <th scope="col">No. Boleto</th>
+                                                <th scope="col">Boletos extra</th>
+                                                <th scope="col">Nombre</th>
+                                                <th scope="col">Apellido</th>
+                                                <th scope="col">No. Whats</th>
+                                                <th scope="col">Estado</th>
+                                                <th scope="col">Estatus</th>
+                                                <th scope="col"></th>
                                             </tr>
-                                        @endforeach
-                                    </tbody>            
-                                </table>       
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($ticketsBuyed as $ticketBuyed)
+                                                <tr>
+                                                    <th scope="row">{{ $ticketBuyed->ticket }}</th>
+                                                    <th>
+                                                        @foreach ($ticketBuyed->otherTicketsBuyed as $item)
+                                                            {{ $item->ticket }}
+                                                        @endforeach                  
+                                                    </th>
+                                                    <td>{{ $ticketBuyed->name_client }}</td>      
+                                                    <td>{{ $ticketBuyed->lastname_client }}</td>                                      
+                                                    <td>{{ $ticketBuyed->whats_number }}</td>                                      
+                                                    <td>{{ $ticketBuyed->state }}</td>                                      
+                                                    <td>
+                                                        @if($ticketBuyed->paid == 1)
+                                                            <strong class="text-success">Pagado</strong> 
+                                                        @else
+                                                            <strong class="text-danger">No pagado</strong>     
+                                                        @endif
+                                                    </td>                                      
+                                                    <td>
+                                                        <a class="btn btn-outline-secondary btn-sm btn-block mb-1" href="{{ route('ticketsBuyed.edit', $ticketBuyed) }}" role="button">Editar</a>
+                                                        <form method="POST" action="{{ route('ticketsBuyed.delete', $ticketBuyed) }}" class="">
+                                                            @csrf
+                                                            <button class="btn btn-outline-danger btn-sm btn-block">Eliminar</button>
+                                                        </form>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>            
+                                    </table>     
+                                </div>  
                             </div>
                         </div>
                     </div>   
