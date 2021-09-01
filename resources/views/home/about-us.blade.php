3f8fb9d2a872974989637da51ea78dc7eb3cb3cd
Irvin López <irvin_21_95@hotmail.com>
2021-09-01 07:34:26 -0600

    add titles

--- resources/views/home/about-us.blade.php
+++ resources/views/home/about-us.blade.php
@@ -1,6 +1,7 @@
 <section class="wrapper bg-light wrapper-border">
-    <div class="container py-10 py-md-16">
-        <div class="row gy-10 gy-sm-13 gx-lg-3 align-items-center">
+    <div class="container">
+        <h2 class="display-4 mb-5 text-center titles" id="nosotros">NOSOTROS</h2>
+        <div class="row gy-10 gy-sm-13 gx-lg-3 align-items-center py-2 py-md-7">
             <div class="col-md-8 col-lg-6 position-relative">
                 <div class="shape bg-dot primary rellax w-17 h-21" data-rellax-speed="1"
                     style="top: -2rem; left: -1.9rem;"></div>
@@ -9,7 +10,6 @@
                 <figure class="rounded"><img src="{{asset($image)}}" alt="" /></figure>
             </div>
             <div class="col-lg-5 col-xl-4 offset-lg-1 text-center">
-                <h2 class="h1 mb-3" id="nosotros">Nosotros</h2>
                 <p class="lead fs-lg mb-6">Rifas Junior</p>
                 <p class="lead fs-lg mb-6">¡Trae para ti los mejores sorteos de toda la república!</p>
                 <p class="lead fs-lg mb-6">Sorteos legales y avalados en base a resultados emitidos por Lotería Nacional Mexicana</p>
