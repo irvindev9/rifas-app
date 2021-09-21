204bb49e01e02cc0a9f15d9c045f201cd541a9ee
Irvin López <irvin_21_95@hotmail.com>
2021-09-20 20:57:22 -0600

    pad left generate

--- resources/views/buy-ticket/_ticket.blade.php
+++ resources/views/buy-ticket/_ticket.blade.php
@@ -10,7 +10,7 @@
                 <div class="px-4 py-2 text-left text-uppercas" style="border-bottom: dashed">
                     <div>
                         <strong class="d-inline">Boleto: </strong>
-                        <p class="d-inline">{{ $ticketBuyed->ticket }}</p>
+                        <p class="d-inline">{{ str_pad($ticketBuyed->ticket, 4, "0", STR_PAD_LEFT) }}</p>
                     </div>
                     <div>
                         <strong class="d-inline">Boletos extras: </strong>
